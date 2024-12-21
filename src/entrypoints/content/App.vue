e68d86a5c4dce118ed4c7eebce71672b0db5b4df
Yutao Dou <yutaodou@gmail.com>
2024-12-22 00:20:47 +0800

    add custom element and tailwindcss

---
+++ src/entrypoints/content/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import "~/assets/tailwind.css";
+import vueLogo from "~/assets/vue.svg";
+</script>
+
+<template>
+  <div class="container">
+    <img :src="vueLogo" />
+    <p>Hello World Good 123</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: fixed;
+  right: 16px;
+  top: 16px;
+  width: 250px;
+  height: 200px;
+  pointer-events: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  border-radius: 8px;
+  color: black;
+}
+
+img {
+  width: 72px;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["src/assets/**", "src/entrypoints/**", "src/components/**"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
