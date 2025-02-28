f10e186f5c35164dee541391439263c4a43c1635
Yutao Dou <ytdou@thoughtworks.com>
2025-02-28 23:56:34 +0800

    feat: add clear search functionality in popup

--- src/entrypoints/popup/App.vue
+++ src/entrypoints/popup/App.vue
@@ -111,6 +111,12 @@ watch(searchQuery, (newValue) => {
   debouncedSearch(newValue);
 });
 
+// Add method to clear search input
+const clearSearch = () => {
+  searchQuery.value = "";
+  searchInput.value?.focus();
+};
+
 onMounted(() => {
   // Focus search input and load initial options when component mounts
   searchInput.value?.focus();
@@ -175,7 +181,13 @@ onUnmounted(() => {
         @compositionend="isComposing = false"
         @keydown="handleKeyDown"
       />
-      <button class="text-gray-400 dark:text-[#9aa0a6] hover:text-gray-600 dark:hover:text-[#e8eaed]">
+      <!-- Replace settings icon with clear icon -->
+      <button 
+        v-if="searchQuery"
+        class="text-gray-400 dark:text-[#9aa0a6] hover:text-gray-600 dark:hover:text-[#e8eaed]"
+        @click="clearSearch"
+        aria-label="Clear search"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           style="width: 20px; height: 20px"
@@ -184,7 +196,7 @@ onUnmounted(() => {
         >
           <path
             fill-rule="evenodd"
-            d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
             clip-rule="evenodd"
           />
         </svg>
