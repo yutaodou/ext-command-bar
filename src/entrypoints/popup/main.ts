46d763a21069bfc65fdb0ceb462b2548dea71dfd
Yutao Dou <ytdou@thoughtworks.com>
2025-05-12 10:45:53 +0800

    refactor: extract command bar component

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import { createApp } from "vue";
+import "./style.css";
+import CommandBar from "../../components/CommandBar.vue";
 
-createApp(App).mount('#app');
+createApp(CommandBar).mount("#app");
